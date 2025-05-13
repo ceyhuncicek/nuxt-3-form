f134900140124138f2111a98ea347fb61067ecf0
ceyhuncicek <cicekceyhun@gmail.com>
2025-05-13 19:32:03 +0200

    Add: packages from prevent cloud and initial page.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
 
+  // Configure for client-side rendering
+  ssr: false,
+
+  // ProvetCloud Design System
+  css: [
+    "@provetcloud/css/lib/provet.css",
+    "@provetcloud/themes/lib/provet.css",
+    "@nordhealth/fonts/lib/fonts.css",
+  ],
+
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/test-utils'
-  ]
-})
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/test-utils",
+  ],
+
+  // Vite configuration for ProvetCloud web components
+  vite: {
+    optimizeDeps: {
+      include: ["@provetcloud/web-components/lib/index.js"],
+    },
+  },
+
+  // Add ProvetCloud web components to client plugins
+  plugins: [{ src: "~/plugins/provetcloud.client.js", mode: "client" }],
+});
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="n-space-y-l">
+    <div class="n-space-y-s">
+      <h1 class="n-typescale-headline-large">Nuxt 3 Form Application</h1>
+      <p class="n-typescale-body">
+        Welcome to the Nuxt 3 Form Application using ProvetCloud Design System
+      </p>
+    </div>
+
+    <RegistrationForm />
+  </div>
+</template>
+
+<script setup></script>
+
+<script>
+// Define the layout for this page
+definePageMeta({
+  layout: "default",
+});
+</script>
